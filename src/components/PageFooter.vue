453f9bc8377111e05d4a52bceeabfef9207da325
wuyunfeng <1363321437@qq.com>
2017-09-24 21:45:06 +0800

    update text

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -13,7 +13,7 @@
       <div class="row">
         <div class="col s4 logo-slogan">
           <div class="logo">
-            优燃+
+            优燃佳
           </div>
           <div class="intro">
             <p>努力成就卓越，卓越造就梦想</p>
@@ -25,12 +25,24 @@
             <div class="col s8">
               <div class="title">团队相关</div>
               <ul class="row">
-                <li class="col s6"><router-link to="/service">产品服务</router-link></li>
-                <li class="col s6"><router-link to="/about">关于我们</router-link></li>
-                <li class="col s6"><router-link to="/contact">联系我们</router-link></li>
-                <li class="col s6"><router-link to="/team">我们团队</router-link></li>
-                <li class="col s6"><router-link to="/job">招贤纳士</router-link></li>
-                <li class="col s6"><router-link to="/step">优燃历程</router-link></li>
+                <li class="col s6">
+                  <router-link to="/service">产品服务</router-link>
+                </li>
+                <li class="col s6">
+                  <router-link to="/about">关于我们</router-link>
+                </li>
+                <li class="col s6">
+                  <router-link to="/contact">联系我们</router-link>
+                </li>
+                <li class="col s6">
+                  <router-link to="/team">我们团队</router-link>
+                </li>
+                <li class="col s6">
+                  <router-link to="/job">招贤纳士</router-link>
+                </li>
+                <li class="col s6">
+                  <router-link to="/step">优燃历程</router-link>
+                </li>
               </ul>
             </div>
             <div class="col s4 qrcode">
@@ -42,24 +54,26 @@
       </div>
     </div>
     <div class="footer-info">
-      <p class="center-align">Copyright © uranplus.com<span>|</span>
+      <p class="center-align">Copyright © uranplus.com
+        <span>|</span>
         <router-link to="/copyright">免责申明</router-link>
         <span>|</span>鄂ICP备130666660号-1
         <span>|</span>QQ咨询:
-        <a target="_bl/explorerank" href="" rel="nofollow">99989999</a></p>
+        <a target="_bl/explorerank" href="" rel="nofollow">99989999</a>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'PageFooter',
-    data () {
-      return {
-        msg: '页脚啊'
-      }
+export default {
+  name: 'PageFooter',
+  data() {
+    return {
+      msg: '页脚啊'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/pages/Front.vue
+++ src/pages/Front.vue
@@ -11,7 +11,9 @@
         <div class="container center-align">
           <div class="slogan">
             <div class="content">
-              <h1><span v-iType></span></h1>
+              <h1>
+                <span v-iType></span>
+              </h1>
             </div>
           </div>
           <div class="contact-box">
@@ -25,7 +27,7 @@
     <div class="section-intro">
       <div class="container center-align">
         <div class="title">
-          <h2>优燃+不止是代码工厂</h2>
+          <h2>优燃佳不止是代码工厂</h2>
           <p>不同于传统的客户以上帝视角给出需求开发方以工厂模式提供产品</p>
           <p>我们提供一整套解决方案，以解决客户痛点为乐趣</p>
         </div>
@@ -40,13 +42,13 @@
             <div class="icon-box">
               <span class="ran-microsoftword"></span>
             </div>
-            <p>从需求出发 剖析客户痛点</p>
+            <p>从需求出发 剖析客户痛点 制定对应解决方案</p>
           </div>
           <div class="col s4">
             <div class="icon-box">
               <span class="ran-redux"></span>
             </div>
-            <p>针对客户痛点 选用相对的技术和解决方案</p>
+            <p>针对解决方案 选用相对的技术栈和实现方案</p>
           </div>
         </div>
       </div>
@@ -84,7 +86,7 @@
               <span class="ran-make-group"></span>
             </div>
             <div class="item-title">推广运营</div>
-            <p>海量的营销资源为您的产品定向推广</p>
+            <p>海量的营销资源为您的产品精准定向推广</p>
           </div>
         </div>
       </div>
@@ -101,21 +103,21 @@
               <span class="ran-stats-dots"></span>
             </div>
             <div class="item-title">营销优势</div>
-            <p>海量的营销资源，了解互联网用户习惯</p>
+            <p>海量的营销资源 了解互联网用户习惯 用户精准营销</p>
           </div>
           <div class="col s4">
             <div class="icon-box">
               <span class="ran-smile"></span>
             </div>
             <div class="item-title">行业优势</div>
-            <p>超多案例，有各行业中大型项目经验</p>
+            <p>丰富经验 提供各个行业的互联网解决方案</p>
           </div>
           <div class="col s4">
             <div class="icon-box">
               <span class="ran-stack"></span>
             </div>
             <div class="item-title">技能优势</div>
-            <p>无可挑剔的创意设计，较为成熟的敏捷开发</p>
+            <p>无可挑剔的创意设计 较为成熟的敏捷开发</p>
           </div>
         </div>
       </div>
@@ -131,10 +133,6 @@
           <div class="col s3">
             <span class="ran-javascript"></span>
             <p>javascript</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-sass"></span>
-            <p>sass</p>
           </div>
           <div class="col s3">
             <span class="ran-vuejs"></span>
@@ -169,6 +167,10 @@
             <p>html5</p>
           </div>
           <div class="col s3">
+            <span class="ran-sass"></span>
+            <p>sass</p>
+          </div>
+          <div class="col s3">
             <span class="ran-git"></span>
             <p>git</p>
           </div>
@@ -184,43 +186,47 @@
         <div class="title">
           <h2>我们的愿景</h2>
           <p>探索新奇 创造无限可能</p>
+          <p>客户满意凝结着我们所有人的辛勤努力</p>
+          <p>我们致力成为中国互联网解决方案行业最值得信赖的合作伙伴</p>
+          <p>我们携手同行，共同致力于中国互联网解决方案事业的美好未来，实现我们一直以来对技术改变世界的美好愿望</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  import Parallax from 'parallax-js';
-  import {init,destroy} from 'ityped';
+import Parallax from 'parallax-js';
+import { init, destroy } from 'ityped';
 
-  export default {
-    name: 'Front',
-    data() {
-      return {
-        msg: '首页'
+export default {
+  name: 'Front',
+  data() {
+    return {
+      msg: '首页'
+    }
+  },
+  directives: {
+    parallax: {
+      inserted(el) {
+        var parallaxInstance = new Parallax(el, {
+          relativeInput: true,
+        });
       }
     },
-    directives: {
-      parallax:{
-        inserted(el){
-          var parallaxInstance = new Parallax(el, {
-            relativeInput: true,
-          });
-        }
-      },
-      iType:{
-        inserted(el){
-          init(el,{
-            strings: [
-              '一个很潮的团队，做着一些很潮的事情',
-              '一个想有梦想的团队',
-              '一个以解决客户痛点为乐趣的团队',
-            ],
-            backDelay: 2000,
-            loop: true,
-          })
-        }
-      },
-    }
+    iType: {
+      inserted(el) {
+        init(el, {
+          strings: [
+            '一个很潮的团队，做着一些很潮的事情',
+            '如果不能使你超越同行，那要我们何用',
+            '一个有梦想的团队',
+            '一个以解决客户痛点为乐趣的团队',
+          ],
+          backDelay: 2000,
+          loop: true,
+        })
+      }
+    },
   }
+}
 </script>
